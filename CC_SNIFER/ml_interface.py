c8193af65b820cce3e68674675616ef57732f558
ArtWachowski <47650079+ArtWachowski@users.noreply.github.com>
2020-08-14 20:49:54 +0100

    Update ml_interface.py

--- CC_SNIFER/ml_interface.py
+++ CC_SNIFER/ml_interface.py
@@ -1,12 +1,17 @@
 import cffi, sys, traceback, pickle, os, string
 import pandas as pd
 import numpy as np
+#import nltk
+#nltk.download('brown')
+#nltk.download('punkt')
+#from nltk.corpus import brown
 from sklearn import preprocessing
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer, TfidfTransformer
 from sklearn_deltatfidf import DeltaTfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
+
 
 ffi = cffi.FFI()
 ffi.cdef(open('ml_interface.h').read())
@@ -20,12 +25,12 @@ def get_prediction(buffer, buffer_len):
         result = ffi.new('char []', data)
 
         #TODO - IF TRAIN MODE ##########SUPERVISED MODE :        
-        traffic_type = "NORMAL" ################### For Trained - Supervised Learning
+        traffic_type = "RESPONSES" ################### For Trained - Supervised Learning
         f_extension = "_training_data.csv"
         f_name = traffic_type + f_extension
         if not os.path.isfile(f_name):
             f = open(f_name, 'w')
-            headers = 'Unique Id, TTL Value, Sum N-Gram Vectorizer, Average N-Gram-Feature, Tfidf Delta, Feature Count,Traffic Type\n'
+            headers = "Unique Id"+"\t"+"TTL Value"+"\t"+"Sum N-Gram Vectorizer"+"\t"+"Average N-Gram-Feature"+"\t"+"Tfidf Delta"+"\t"+"Feature Count"+"\t"+"N-GRAMS_3"+"\t"+"N-GRAMS_DELTA_3"+"\t"+"N-GRAMS_4"+"\t"+"N-GRAMS_DELTA_4"+"\t"+"N-GRAMS_5"+"\t"+"N-GRAMS_DELTA_5"+"\t"+"N-GRAMS_6"+"\t"+"N-GRAMS_DELTA_6"+"\t"+"Traffic Type"+"\n"
             f.write(headers)
             f.close
             print("Generating new file",f_name)
@@ -86,6 +91,9 @@ def ml_processing(data,traffic_type):
         data = data.split(b',')
         ml_packet = [f.decode(encoding='utf-8', errors='strict') for f in data]
 
+        if ml_packet[8] is None:
+            return
+
         if len(ml_packet[8]) <= 6: # TODO Fix MMS on C side
             print("ML Packet must be minimum 6 len: ", len(ml_packet) )
             print("\ Python ML_bytes.......", ml_bytes) 
@@ -107,12 +115,12 @@ def ml_processing(data,traffic_type):
           
     except:
         print('Def_Preprocessing Terrible Error!')
-        traceback.print_exc()
+        #traceback.print_exc()
 
     return ml_bytes,unique_id,ttl,id
 
 def randomizator(ml_bytes,unique_id,ttl,id,f_name,traffic_type):
-    
+   
     if len(ml_bytes[0]) < 6:
         print("Packet must be minimum 6 byte len to meet N-Gram _ TODO")
         print("\ Python ML_bytes.......", ml_bytes) 
@@ -124,6 +132,53 @@ def randomizator(ml_bytes,unique_id,ttl,id,f_name,traffic_type):
         deltaV = X.toarray()
         deltaV.astype(float)
         deltaV = np.sum(deltaV)
+
+        #NGRAMS ...............   
+        ngram3 = CountVectorizer(ngram_range=(3,3))
+        X3 = ngram3.fit_transform(ml_bytes)
+        n3= ngram3.get_feature_names()
+        n3= ''.join([str(elem) for elem in n3])
+        
+        #print("\...........", natural_language(n3))
+
+        delta3 = X3.toarray()
+        delta3.astype(float)
+        d3 = np.sum(delta3)
+
+        ngram4 = CountVectorizer(ngram_range=(4,4))
+        X4 = ngram4.fit_transform(ml_bytes)
+        n4= ngram4.get_feature_names()
+        n4= ''.join([str(elem) for elem in n4])
+
+        #print("\...........", natural_language(n4))
+
+
+        delta4 = X4.toarray()
+        delta4.astype(float)
+        d4 = np.sum(delta4)
+
+        ngram5 = CountVectorizer(ngram_range=(5,5))
+        X5 = ngram5.fit_transform(ml_bytes)
+        n5= ngram5.get_feature_names()
+        n5= ''.join([str(elem) for elem in n5])
+
+        #print("\...........", natural_language(n5))
+
+        delta5 = X5.toarray()
+        delta5.astype(float)
+        d5 = np.sum(delta5)
+
+        ngram6 = CountVectorizer(ngram_range=(6,6))
+        X6 = ngram6.fit_transform(ml_bytes)
+        n6= ngram6.get_feature_names()
+        n6= ''.join([str(elem) for elem in n6])
+
+        #print("\...........", natural_language(n6))
+
+
+        delta6 = X6.toarray()
+        delta6.astype(float)
+        d6 = np.sum(delta6)
 
         victor2 = HashingVectorizer()
         X = victor2.fit_transform(features_names)
@@ -143,7 +198,9 @@ def randomizator(ml_bytes,unique_id,ttl,id,f_name,traffic_type):
         deltaT.astype(float)
         deltaT = np.sum(deltaT)
 
-        new_string = str(unique_id)+","+ttl+","+str(deltaV)+","+str(int(deltaV)/int(hashingVectorizer))+","+str(deltaT)+","+hashingVectorizer+","+traffic_type
+        ng=str(n3)+"\t"+str(d3)+"\t"+str(n4)+"\t"+str(d4)+"\t"+str(n5)+"\t"+str(d5)+"\t"+str(n6)+"\t"+str(d6)
+        new_string=str(unique_id)+"\t"+ttl+"\t"+str(deltaV)+"\t"+str(int(deltaV)/int(hashingVectorizer))+"\t"+str(deltaT)+"\t"+hashingVectorizer+"\t"+ng+"\t"+traffic_type
+
         print(id,"- New Record", new_string)
 
         write_to_file(new_string,f_name)
@@ -151,6 +208,21 @@ def randomizator(ml_bytes,unique_id,ttl,id,f_name,traffic_type):
     except:
         print('Even More Terrible Error!')
         traceback.print_exc()
+
+def natural_language(raw):
+
+    #ref: https://www.nltk.org/book/ch05.html#sec-dictionaries
+
+    bs = brown.tagged_sents(categories='news') #### TODO Regex This 
+    b = brown_sents = brown.sents(categories='news')
+    tokens = nltk.word_tokenize(raw)
+    #tagger = nltk.DefaultTagger('NN')
+    tagger = nltk.UnigramTagger(bs)
+    tagger.tag(tokens)
+
+    ni=tagger.evaluate(bs)
+
+    return ni ## Example Value 0.13089484257215028
 
 
 def write_to_file(new_string,f_name):
